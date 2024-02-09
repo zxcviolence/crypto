39db8d597cae7af5db1aee30158a3f0c3079d07c
Islam <yakubov959@mail.ru>
2024-02-09 18:13:27 +0300

    modal info

--- src/components/CoinInfoModal.jsx
+++ src/components/CoinInfoModal.jsx
@@ -1,3 +1,54 @@
+import { Flex, Typography, Tag, Divider } from "antd";
+
 export const CoinInfoModal = ({ coin }) => {
-  return <h2>{coin.name}</h2>;
+  return (
+    <>
+      <Flex align="center">
+        <img
+          src={coin.icon}
+          alt={coin.name}
+          style={{ width: 40, marginRight: 10 }}
+        />
+        <Typography.Title level={2} style={{ margin: 0 }}>
+          ({coin.symbol}) {coin.name}
+        </Typography.Title>
+      </Flex>
+      <Divider />
+      <Typography.Paragraph>
+        <Typography.Text strong>1 hour: </Typography.Text>
+        <Tag color={coin.priceChange1h > 0 ? "green" : "red"}>
+          {coin.priceChange1h}%
+        </Tag>
+        <Typography.Text strong>1 day: </Typography.Text>
+        <Tag color={coin.priceChange1d > 0 ? "green" : "red"}>
+          {coin.priceChange1d}%
+        </Tag>
+        <Typography.Text strong>1 week: </Typography.Text>
+        <Tag color={coin.priceChange1w > 0 ? "green" : "red"}>
+          {coin.priceChange1w}%
+        </Tag>
+      </Typography.Paragraph>
+      <Typography.Paragraph>
+        <Typography.Text strong>
+          Price: {coin.price.toFixed(2)}$
+        </Typography.Text>
+      </Typography.Paragraph>
+
+      <Typography.Paragraph>
+        <Typography.Text strong>Price BTC: {coin.priceBtc}</Typography.Text>
+      </Typography.Paragraph>
+
+      <Typography.Paragraph>
+        <Typography.Text strong>Market Cap: {coin.marketCap}$</Typography.Text>
+      </Typography.Paragraph>
+
+      {coin.contractAddress && (
+        <Typography.Paragraph>
+          <Typography.Text strong>
+            Contract Address: {coin.contractAddress}
+          </Typography.Text>
+        </Typography.Paragraph>
+      )}
+    </>
+  );
 };
