1a7f54c43b72c8b4da4a7869ef60687364c5a92e
Islam <yakubov959@mail.ru>
2024-02-12 15:56:14 +0300

    content part

--- src/components/Layout/AppContent/index.jsx
+++ src/components/Layout/AppContent/index.jsx
@@ -1,8 +1,25 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useCrypto } from "../../../hooks/useCrypto";
 import styles from "./content.module.scss";
 
 export const AppContent = () => {
+  const { assets, crypto } = useCrypto();
+
+  const cryptoPriceMap = crypto.reduce((acc, coin) => {
+    acc[coin.id] = coin.price;
+    return acc;
+  }, {});
+
   return (
-    <Layout.Content className={styles.contentStyle}>Content</Layout.Content>
+    <Layout.Content className={styles.contentStyle}>
+      <Typography.Title level={3} style={{ textAlign: "left", color: "#fff" }}>
+        Portfolio{" "}
+        {assets
+          .map(asset => asset.amount * cryptoPriceMap[asset.id])
+          .reduce((acc, v) => (acc += v), 0)
+          .toFixed(2)}
+        $
+      </Typography.Title>
+    </Layout.Content>
   );
 };
